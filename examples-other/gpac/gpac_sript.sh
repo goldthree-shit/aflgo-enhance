4b242d40f7a19e47e1bb1d1e50205229b998c63c
hx <1548973853@qq.com>
2025-02-16 23:30:57 +0800

    update

--- examples-other/gpac/gpac_sript.sh
+++ examples-other/gpac/gpac_sript.sh
@@ -15,7 +15,7 @@ python3 $AFLGO/scripts/git_line_changes.py --repo_path $SUBJECT \
                  --version f40aaaf959d4d1f7fa0dcd04c0666592e615c8f1
 # 编译
 cd obj-aflgo; CFLAGS="$ADDITIONAL" CXXFLAGS="$ADDITIONAL" ../configure --disable-shared --prefix=`pwd`
-make clean; make -j4
+make clean; make -j
 # 生成BBcall，BBnames
 cat $TMP_DIR/BBnames.txt | rev | cut -d: -f2- | rev | sort | uniq > $TMP_DIR/BBnames2.txt && mv $TMP_DIR/BBnames2.txt $TMP_DIR/BBnames.txt
 cat $TMP_DIR/BBcalls.txt | sort | uniq > $TMP_DIR/BBcalls2.txt && mv $TMP_DIR/BBcalls2.txt $TMP_DIR/BBcalls.txt
@@ -24,18 +24,25 @@ python3 $AFLGO/distance/gen_distance_fast.py $SUBJECT/obj-aflgo/bin/gcc $TMP_DIR
 # 重新编译
 make clean; make distclean
 CFLAGS="-distance=$TMP_DIR/distance.cfg.txt" CXXFLAGS="-distance=$TMP_DIR/distance.cfg.txt" ../configure --disable-shared --prefix=`pwd`
-make -j4
-# 生成种子
-mkdir in
-# /tests/media 下的文件全都拷贝到一个文件夹中
-find "$SUBJECT/tests/media" -type f -exec cp {} "in" \;
-# 对于相同的文件类型随机保留两个
-find "in" -type f | sed -n 's/.*\.\([^.]*\)$/\1/p' | sort | uniq | while read ext; do
-  find "in" -type f -name "*.$ext" | shuf | tail -n +3 | xargs rm -f
-done
+make -j
 # 添加动态库
 LIBR_PATHS=$(find $(pwd)/bin/gcc -maxdepth 1 -type d)
 export LD_LIBRARY_PATH=$(echo $LIBR_PATHS | tr ' ' ':')
+# 生成种子
+mkdir in  
+# 1. 复制测试媒体文件
+find "$SUBJECT/tests/media" -type f -exec cp {} "in" \;
+# 2. 按内容去重 (使用 afl-cmin)
+# 注意要根据测试的选项灵活调整 -diso -out /dev/null 中的内容
+$AFLGO/afl-2.57b/afl-cmin -i in -o in_unique -- ./bin/gcc/MP4Box -diso -out /dev/null @@
+rm -rf in && mv in_unique in
+# 3. 最小化初始种子
+# 注意要根据测试的选项灵活调整 -diso -out /dev/null 中的内容
+mkdir -p minimized_seeds 
+for input_file in in/*; do filename=$(basename $input_file); 
+  $AFLGO/afl-2.57b/afl-tmin -i $input_file -o minimized_seeds/$filename -- ./bin/gcc/MP4Box -diso -out /dev/null @@; 
+done
+rm -rf in && mv minimized_seeds in
 # 在关键字段中插入随机数据（如 moov box）
 # -m zone: 无内存限制
 # -z exp: 深度探索模式 ； fast：快速模式
